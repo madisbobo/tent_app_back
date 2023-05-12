92451fde44f4f1416c799aa3df633dfc08ad8dde
Madis Bobrovski <115641439+madisbobo@users.noreply.github.com>
2023-05-12 17:01:06 +0300

    Initial commit

---
+++ src/main/java/ee/valiit/tent_app_back/TentAppBackApplication.java
@@ -0,0 +1,13 @@
+package ee.valiit.tent_app_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TentAppBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TentAppBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/valiit/tent_app_back/TentAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.valiit.tent_app_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TentAppBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
